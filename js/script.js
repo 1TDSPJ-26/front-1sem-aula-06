4392b44a831201f5284c9e330c8e82c0540cb26b
matheusfa08 <littlepacoquinha@gmail.com>
2026-05-14 11:20:45 -0300

    feat: Métodos de String, comparação com listas. Criando listas de objetos manipulando elementos, explicando a diferença entre funções.

--- js/script.js
+++ js/script.js
@@ -112,5 +112,131 @@ imgAvatar.alt = usuario.nome
 imgAvatar.width = 40
 */
 
-//Para casa:
-//Recuperar os dados dos campos do FORM e armazenar em um objeto e imprimir eles assim que o botão Entrar for clicado!!
+//Recuperando dados dos campos da form e amarzenando em um objeto assim que o botão entrar dor clicado
+
+//Conhecendo as Strings
+
+/*let frase = "Olá, mundo!";
+console.log(frase);
+
+//Contando os caractéres de uma string
+
+console.log(frase.length);
+console.log(frase[2]);
+
+//Realizando a leitura dinâmica de uma string
+
+for(let x = 0; x < frase.length; x++){
+    console.log(frase[x]);
+    
+    if(frase[x].toLocaleLowerCase() === "o"){
+        console.log("Encontrei um o na posição " + x);
+    }
+}*/
+
+
+//console.log(usuarios);
+//console.table(usuarios);
+
+//funções em JavaScript
+
+/*function login(){
+    //contexto da função
+    let resultado = "Login realizado com sucesso!";
+    resultado = "Login falhou! Verifique suas credenciais.";
+    return resultado;
+    }
+    
+    console.log(login());
+    */
+   
+   //const botaoentrar = document.getElementById("btn-entrar");
+   
+   /*botaoentrar.addEventListener("click", function(event){
+    console.log(this);
+    })*/
+    const usuarios = [
+      {
+        email: "zarak.vultos@exemplo.com",
+        senha: "12345",
+        nome: "Zarak dos Vultos",
+        idade: 42,
+        avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+      },
+      {
+        email: "elara.astral@exemplo.com",
+        senha: "12345",
+        nome: "Elara Galáxia Astral",
+        idade: 27,
+        avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+      },
+      {
+        email: "mordecai.bruma@exemplo.com",
+        senha: "12345",
+        nome: "Mordecai da Bruma",
+        idade: 51,
+        avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+      },
+      {
+        email: "thallos.ferro@exemplo.com",
+        senha: "12345",
+        nome: "Thallos de Ferro",
+        idade: 35,
+        avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+      },
+      {
+        email: "lyra.eco@exemplo.com",
+        senha: "12345",
+        nome: "Lyra Silvestre Eco",
+        idade: 22,
+        avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+      },
+      {
+        email: "boros.fenda@exemplo.com",
+        senha: "12345",
+        nome: "Boros da Fenda Profunda",
+        idade: 64,
+        avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+      },
+      {
+        email: "vaelin.nimbus@exemplo.com",
+        senha: "12345",
+        nome: "Vaelin Nimbus",
+        idade: 30,
+        avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+      },
+      {
+        email: "selene.quartzo@exemplo.com",
+        senha: "12345",
+        nome: "Selene de Quartzo",
+        idade: 29,
+        avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+      },
+      {
+        email: "kaelthas.sol@exemplo.com",
+        senha: "12345",
+        nome: "Kaelthas Brilho do Sol",
+        idade: 47,
+        avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+      },
+      {
+        email: "joric.martelo@exemplo.com",
+        senha: "12345",
+        nome: "Joric Martelo de Pedra",
+        idade: 38,
+        avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+      }
+    ];
+
+    /*
+    usuarios.forEach((u, i)=>{
+        console.log(i, u.nome);
+    });
+    */
+
+    //capturando o botão de entrar para iniciar a tarefa de login
+    const btnEntrar = document.getElementById("btn-entrar");
+    //atrelando um evento ao botão de entrar para iniciar a tarefa de validação do login
+    btnEntrar.addEventListener("click", ()=>{
+
+    });
